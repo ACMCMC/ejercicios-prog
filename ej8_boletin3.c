8b6f69b40adcdcfd4c2564fa5d493d23a5e240de
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-11-24 23:58:40 +0100

    Co-authored-by: portela sobrino sandra <sandra.portela@rai.usc.es>

--- ej8_boletin3.c
+++ ej8_boletin3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define D 20
 
 typedef struct{
     char nconta[25];
@@ -14,7 +13,7 @@ typedef struct{
 
 void consultarusuario(){
     char dni[10];  //porque un dni tiene 9 letras y un /0 al final
-    int cmp; /////////borrar//////
+    int cmp;
     int condicion;
     cliente c;
     FILE* idf;
@@ -25,16 +24,16 @@ void consultarusuario(){
     else{
         printf("introduce el dni del usuario:");
         scanf(" %s",dni);
-        fseek(idf,0,SEEK_SET); // Nos ponemos al principio
-            condicion=!feof(idf);
-            while (condicion) { //leer, comprobar si el dni de las personas es igual al que escribieron antes,comprobar si estamos al final del archivo
-                fread(&c,sizeof(cliente),1,idf);
-                cmp=strcmp(c.dni,dni);
-                if(cmp==0){
-                    printf(" %s %s %s %s %s %f",c.nconta,c.nome,c.apelidos,c.dni,c.enderezo,c.saldo);
-                }
-                condicion=!feof(idf) && !cmp;
+        fseek(idf,0,SEEK_SET); //  poñemonos ao principio
+        condicion=!feof(idf);
+        while (condicion) { //leer, comprobar se o dni das personas e igual ao que escribiron antes,comprobar si estamos ao final del arquivo
+            fread(&c,sizeof(cliente),1,idf);
+            cmp=strcmp(c.dni,dni);
+            if(cmp==0){
+                printf(" %s %s %s %s %s %f\n",c.nconta,c.nome,c.apelidos,c.dni,c.enderezo,c.saldo);
             }
+            condicion=!feof(idf) && cmp;
+        }
     }
     fclose(idf);
 }
@@ -47,16 +46,20 @@ void contanova(){
     }
     else{
         printf("introduce a conta\n");
-        scanf(" %s", c.nconta); //metemos el resto
-        printf("introduce o enderezo\n");
-        scanf(" %s", c.enderezo); //metemos el resto
+        scanf(" %s", c.nconta);
+        printf("introduce o nome\n");
+        scanf(" %s",c.nome);getchar();
+        printf("introduce os apelidos\n");
+        gets(c.apelidos);
         printf("introduce o dni\n");
-        scanf(" %s", c.dni); //metemos el resto
+        scanf(" %s", c.dni);getchar();
+        printf("introduce o enderezo\n");
+        gets( c.enderezo);
         printf("introduce o saldo\n");
-        scanf(" %f", &(c.saldo)); //metemos el resto
+        scanf(" %f", &(c.saldo));
         fseek(idf,0,SEEK_END);
         fwrite(&c,sizeof(cliente),1,idf);
-    }//al modificar datos estamos leyendo un struct, al leer un struct vamos a querer un fseek  para situarnos al final del archivo
+    }
     fclose(idf);
 }
 void modificarsaldo(){
@@ -77,18 +80,17 @@ void modificarsaldo(){
                 printf("para ingresar pulse 1, para retirar pulse 2");
                 scanf(" %d",&opcion);
                 if (opcion==1) {
-                        printf("cantidad que desea ingresar:"); scanf(" %f",&dinero);
-                        c.saldo+=dinero;
-                        fwrite(&c,sizeof(cliente),1,idf), sizeof(cliente);
-                        return;
-            } else if (opcion==2) {
-                        printf("cantidad que desea retirar:"); scanf(" %f",&dinero);
-                        c.saldo-=dinero;
-                        fwrite(&c,sizeof(cliente),1,idf);
-                        return;
-            } else {
-                return; //CAMBIAR
-            }
+                    printf("cantidad que desea ingresar:"); scanf(" %f",&dinero);
+                    c.saldo+=dinero;
+                    fwrite(&c,sizeof(cliente),1,idf);
+                    return;
+                }
+                else if (opcion==2) {
+                    printf("cantidad que desea retirar:"); scanf(" %f",&dinero);
+                    c.saldo-=dinero;
+                    fwrite(&c,sizeof(cliente),1,idf);
+                    return;
+                }
             }
             else printf("non se atopou o dni");
         }
@@ -96,30 +98,34 @@ void modificarsaldo(){
     fclose(idf);
 }
 void enderezo(){
-    /*char dni[10];
-    int i,cmp;
-    if(idf=fopen("archivo.bin","r+b")==NULL) printf("erro");
+    FILE* idf;
+    cliente c;
+    char dni[10];
+    int cmp, flag_cambiado;
+    if((idf=fopen("archivo.bin","rb+"))==NULL) printf("erro");
     else{
         printf("introduce o dni de quen queres cambiar o enderezo");
-        scanf(" %s",&dni);
-        while(feof(idf)){
-            fread(&c[i],sizeof(cliente),6,idf);
-            cmp=strcmp(c[i].dni,dni);
+        scanf(" %s",dni);
+        flag_cambiado=1;
+        while(flag_cambiado){
+            fread(&c,sizeof(cliente),1,idf);
+            flag_cambiado = !feof(idf);
+            cmp=strcmp(c.dni,dni);
             if(cmp==0){
+                fseek(idf,(-1*sizeof(cliente)),SEEK_CUR);
                 printf("introduce o novo enderezo");
-                gets(&c[i].enderezo);
-                fwrite(&c[i].enderezo,sizeof(char),15,idf);
+                gets(c.enderezo);
+                fwrite(&c,sizeof(cliente),1,idf);
+                flag_cambiado=0;
             }
-            i++;
         }
     }
-    fclose(idf);*/
+    fclose(idf);
 }
- //fwrite(&c,cliente.dni)
 int main() {
-    int opcion,i;
+    int opcion;
     do{
-        printf("que operacion quieres realizar?\n");
+        printf("\nque operacion quieres realizar?\n");
         printf("se queres leer dni pulsa 1\n");
         printf("se queeres engadir unha nova conta pulsa 2\n");
         printf("se queres engadir ou retirar diñeiro pulsa 3\n");
